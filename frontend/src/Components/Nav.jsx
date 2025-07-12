ab48bcf0a9e10e226e6d9bef35e1746f6230d3f5
mqkhan122 <mqasimkhan122@gmail.com>
2025-07-12 06:00:58 -1200

    css

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import '../App.css'
 import logo from '../assets/pngwing.com (1).png'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoServerOutline } from "react-icons/io5";
 const Nav = () => {
   return (
     <>
+    <input type="checkbox" id='click' />
      <section className='flex bg-gray-50 justify-between px-10 items-center shadow-2xs '>
         <div>
             <img src={logo} width={"130px"} alt="" />
         </div>
 
-        <div>
+        <div className='nav-ul'>
             <ul className='flex gap-5 font-semibold'>
                 <li>Home</li>
                 <li>Experince</li>
@@ -21,7 +23,9 @@ const Nav = () => {
         <div className='flex gap-2 items-center '>
             <h4>Become a Host</h4>
             <IoServerOutline className='w-9 bg-gray-200 h-9 p-2 rounded-2xl' />
-            <GiHamburgerMenu className='w-9 bg-gray-200 h-9 p-2 rounded-2xl' />
+            <label htmlFor="click">
+                <GiHamburgerMenu className='w-9 bg-gray-200 h-9 p-2 rounded-2xl' />
+            </label>
         </div>
      </section>
     </>
