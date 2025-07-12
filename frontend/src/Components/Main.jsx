e3b5cafd1f0f74d41e6090c24bb1b0930cf87b57
mqkhan122 <mqasimkhan122@gmail.com>
2025-07-12 06:07:07 -1200

    res

--- frontend/src/Components/Main.jsx
+++ frontend/src/Components/Main.jsx
@@ -5,9 +5,9 @@ const Main = () => {
     <>
       <h1 className='font-semibold text-xl mt-5 px-3'>Popular Homes in Bhopal </h1>
 
-      <section className='col lg:flex p-4'>
+      <section className='col lg:flex justify-items-center items-center p-4'>
         <div className='rounded  p-2'>
-            <img src="https://a0.muscache.com/im/pictures/hosting/Hosting-1321618195220660866/original/3bd5c0e2-c7af-45e4-82f5-0564f86b3ccc.jpeg?im_w=320" alt="" className='rounded-4xl h-44    w-90' />
+            <img src="https://a0.muscache.com/im/pictures/hosting/Hosting-1321618195220660866/original/3bd5c0e2-c7af-45e4-82f5-0564f86b3ccc.jpeg?im_w=320" alt="" className='rounded-4xl h-44  w-90' />
             <h1 className='font-semibold'>Guest hourse in Majorda</h1>
             <h3 className='text-gray-600'>Rs 3,253/- for 2 nights</h3>
         </div>
@@ -40,7 +40,7 @@ const Main = () => {
 
        <h1 className='font-semibold text-xl mt-5 px-3'>Available in next month in South Goa </h1>
 
-      <section className='col lg:flex p-4'>
+      <section className='col lg:flex justify-items-center items-center p-4'>
         <div className='rounded  p-2'>
             <img src="https://a0.muscache.com/im/pictures/miso/Hosting-1104114230621090130/original/f7920495-8d00-447a-a0ac-e102f639babe.jpeg?im_w=720" alt="" className='rounded-4xl h-44    w-90' />
             <h1 className='font-semibold'>Guest hourse in Majorda</h1>
