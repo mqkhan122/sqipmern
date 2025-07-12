de5c39a1bb1e9ac7901f361b4ae1e051e4088c46
mqkhan122 <mqasimkhan122@gmail.com>
2025-07-12 05:51:44 -1200

    first

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <section className='col lg:flex gap-9 justify-evenly p-5 bg-gray-100 leading-8'>
+      <div>
+        <h1 className='font-semibold'>Support</h1>
+        <ul>
+          <li>Help Centre</li>
+          <li>AirCover</li>
+          <li>Anti-discrimination</li>
+          <li>Disability support</li>
+          <li>Cancellation options</li>
+          <li>Report neighbourhood concern</li>
+        </ul>
+       </div>
+
+       <div>
+        <h1 className='font-semibold'>Support</h1>
+        <ul>
+          <li>Help Centre</li>
+          <li>AirCover</li>
+          <li>Anti-discrimination</li>
+          <li>Disability support</li>
+          <li>Cancellation options</li>
+          <li>Report neighbourhood concern</li>
+        </ul>
+       </div>
+
+       <div>
+        <h1 className='font-semibold'>Support</h1>
+        <ul>
+          <li>Help Centre</li>
+          <li>AirCover</li>
+          <li>Anti-discrimination</li>
+          <li>Disability support</li>
+          <li>Cancellation options</li>
+          <li>Report neighbourhood concern</li>
+        </ul>
+       </div>
+     </section>
+
+    </>
+  )
+}
+
+export default Footer
